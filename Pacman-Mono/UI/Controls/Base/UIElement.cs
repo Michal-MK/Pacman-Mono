aaf75ca99985233630252a647e932a854ad11575
Michal-MK <michalhz159@gmail.com>
2021-06-12 01:06:22 +0200

    Introduced UIElement and made Button use it

---
+++ Pacman-Mono/UI/Controls/Base/UIElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.UI.Enums;
+
+namespace MonoGame.UI.Controls.Base {
+	public class UIElement {
+
+		protected Point Position { get; }
+		protected Vector2 PositionV2 => Position.ToVector2();
+		protected OriginMode OriginMode { get; }
+
+		protected Texture2D MainTexture { get; }
+		protected bool IsMouseOver { get; set; }
+
+		public event EventHandler<MouseState> MouseOver;
+
+		protected readonly Vector2 originOffset;
+
+		protected UIElement(Point origin, string textureID, OriginMode mode) {
+			Position = origin;
+			OriginMode = mode;
+			MainTexture = Game.Sprites[textureID];
+
+			if (mode == OriginMode.Center) {
+				originOffset = MainTexture.Bounds.Size.ToVector2() * 0.5f;
+			}
+			else {
+				originOffset = Vector2.Zero;
+			}
+		}
+
+		public virtual void Update(GameTime time) {
+			MouseState state = Mouse.GetState();
+			(float x, float y) = PositionV2 - originOffset;
+
+			if (state.X >= x && state.Y >= y &&
+				state.X <= x + MainTexture.Width &&
+				state.Y <= y + MainTexture.Height) {
+				IsMouseOver = true;
+				MouseOver?.Invoke(this, state);
+			}
+			else {
+				IsMouseOver = false;
+			}
+		}
+
+		public virtual void Draw(GameTime time, SpriteBatch batch) { }
+	}
+}
--- Pacman-Mono/UI/Controls/Button.cs
+++ Pacman-Mono/UI/Controls/Button.cs
@@ -2,72 +2,49 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.UI.Controls.Base;
 using MonoGame.UI.Enums;
 
 namespace MonoGame.UI.Controls {
-	public class Button {
+	public class Button : UIElement {
 
 		public const string TEXTURE_ID = "button";
 
-
 		public event EventHandler OnClick;
 
-		public string ButtonText { get => _buttonText; set { _buttonText = value; textHalfSize = Game.Font.MeasureString(value) * 0.5f; } }
+		private string buttonText;
+		public string ButtonText { get => buttonText; set { buttonText = value; textHalfSize = Game.Font.MeasureString(value) * 0.5f; } }
 
-		private string _buttonText;
 		private Vector2 textHalfSize;
-		private Point buttonPos;
-		private Vector2 buttonOriginOffset;
-		private readonly Texture2D buttonTexture;
-		private OriginMode originMode;
 
 		private ButtonState activeState;
 
-		private bool isMouseOver;
-
-		public Button(Point origin, string text, OriginMode mode) {
-			buttonTexture = Game.Sprites[TEXTURE_ID];
-			buttonPos = origin;
+		public Button(Point origin, string text, OriginMode mode) : base(origin, TEXTURE_ID, mode) {
 			ButtonText = text;
-			textHalfSize = Game.Font.MeasureString(text) * 0.5f;
-			if (mode == OriginMode.Center) {
-				buttonOriginOffset = buttonTexture.Bounds.Size.ToVector2() * 0.5f;
-			}
-			else {
-				buttonOriginOffset = Vector2.Zero;
-			}
+			MouseOver += OnMouseOver;
 		}
 
-		public void Update(GameTime time) {
-			MouseState state = Mouse.GetState();
-			Vector2 renderPos = buttonPos.ToVector2() - buttonOriginOffset;
-
-			if (state.X >= renderPos.X && state.Y >= renderPos.Y &&
-			   state.X <= renderPos.X + buttonTexture.Width &&
-			   state.Y <= renderPos.Y + buttonTexture.Height) {
-				if (state.LeftButton == ButtonState.Pressed) {
+		private void OnMouseOver(object sender, MouseState e) {
+			switch (e.LeftButton) {
+				case ButtonState.Pressed:
 					activeState = ButtonState.Pressed;
-				}
-				if (state.LeftButton == ButtonState.Released && activeState == ButtonState.Pressed) {
-					OnClick?.Invoke(this, System.EventArgs.Empty);
+					break;
+				case ButtonState.Released when activeState == ButtonState.Pressed:
+					OnClick?.Invoke(this, EventArgs.Empty);
 					activeState = ButtonState.Released;
-				}
-				isMouseOver = true;
-			}
-			else {
-				isMouseOver = false;
-				activeState = ButtonState.Released;
+					break;
+				default:
+					activeState = ButtonState.Released;
+					break;
 			}
 		}
 
-		public void Draw(GameTime time, SpriteBatch batch) {
-			batch.Draw(buttonTexture, buttonPos.ToVector2() - buttonOriginOffset,
-					   buttonTexture.Bounds, isMouseOver ? Color.Green : Color.White, 0,
-					   buttonTexture.Bounds.Location.ToVector2(), 1, SpriteEffects.None, 0);
+		public override void Draw(GameTime time, SpriteBatch batch) {
+			batch.Draw(MainTexture, PositionV2 - originOffset,
+				MainTexture.Bounds, IsMouseOver ? Color.Green : Color.White, 0,
+				MainTexture.Bounds.Location.ToVector2(), 1, SpriteEffects.None, 0);
 
-			batch.DrawString(Game.Font, ButtonText, buttonPos.ToVector2() - buttonOriginOffset +
-							 buttonTexture.Bounds.Size.ToVector2() * 0.5f - textHalfSize,
-							 Color.DarkRed);
+			batch.DrawString(Game.Font, ButtonText, Position.ToVector2() - textHalfSize, Color.DarkRed);
 		}
 	}
 }
